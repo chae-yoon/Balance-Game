f74fce27f539bf939077759a3daf7f786e446c4c
chae-yoon <munimooo9765@gmail.com>
2023-04-20 10:32:20 +0900

    Create profile, update, change_password page

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import update_session_auth_hash
+
 # Create your views here.
 def login(request):
     if request.user.is_authenticated:
@@ -39,3 +42,52 @@ def signup(request):
 def logout(request):
     auth_logout(request)
     return redirect('posts:index')
+
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+@login_required
+def follows(request, user_pk):
+    User = get_user_model()
+    person = User.objects.get(pk=user_pk)
+    if request.user != person:
+        if person.followers.filter(pk=request.user.pk).exists():
+            person.followers.remove(request.user)
+        else:
+            person.followers.add(request.user)
+    return redirect('accounts:profile', person.username)
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:profile', request.user.username)
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('accounts:profile', user.username)
+    
+    form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
