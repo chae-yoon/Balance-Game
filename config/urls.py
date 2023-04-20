96a3cff66a071b219f3c0af1320d84e80bb4575e
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-20 13:34:46 +0900

    Add url link & image in detail

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.redirect_index),
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
